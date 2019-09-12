93dd43547bc170609eb3a8999e3e3f994550ff59
KriegersBlog <53866271+KriegersBlog@users.noreply.github.com>
2019-09-12 19:42:32 +0200

    Added sample files

---
+++ main_code/main_code/main_code.ino
@@ -0,0 +1,15 @@
+/* Author: Niklas Kamm, Julian Krieger, Pascal Gläß
+ * License: See github.com/KriegersBlog/color_sorting_machine
+ * 
+ * This code is optimized to work on a Arduino Uno R3 to manage
+ * the whole color sorting process. For more information: See documentation
+ */
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
